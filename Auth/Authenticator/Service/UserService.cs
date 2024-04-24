97d75c1d02e030019c70f3d7a61a32ddbee7002c
neverloved-dev <djordjeandjic99@gmail.com>
2024-04-24 18:13:48 +0100

    code refactor

--- Auth/Authenticator/Service/UserService.cs
+++ Auth/Authenticator/Service/UserService.cs
@@ -8,7 +8,7 @@ public class UserService
     private UserDataContext _dataContext;
     public GetUserDTO getUserByEmail(string userEmail)
     {
-        var user = _dataContext.Users.Where(s=>s.Email == userEmail).First();
+        var user = FindUserObjectByEmail(userEmail);
         var userDto = user.MapToGetDTO();
         return userDto;
     }
