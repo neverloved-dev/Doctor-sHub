f4bf3e7bb7701a4bd9f9ddfccb92d18255525152
neverloved-dev <djordjeandjic99@gmail.com>
2024-05-06 17:21:19 +0100

    change token generation

--- Auth/Authenticator/Service/TokenService.cs
+++ Auth/Authenticator/Service/TokenService.cs
@@ -34,7 +34,7 @@ public class TokenService
         }
     }
 
-    public string CreateToken(User user)
+    public string CreateToken(User user,Roles role)
     {
         var tokenKeyValue = _configuration.GetSection("AppSettings:Token").Value;
         if (string.IsNullOrEmpty(tokenKeyValue))
@@ -47,7 +47,7 @@ public class TokenService
         var token = new JwtSecurityToken(
             claims: new[]
             {
-                new Claim(ClaimTypes.Role, user.Name),
+                new Claim(ClaimTypes.Role, role.ToString()),
             },
             expires: DateTime.Now.AddDays(10),
             signingCredentials: cred
