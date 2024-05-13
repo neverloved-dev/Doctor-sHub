69c16063cee81cde6efd3a7645b352920d1a066b
neverloved-dev <djordjeandjic99@gmail.com>
2024-05-13 16:53:59 +0100

    Update git workflow

---
+++ Main-service/Main/Controllers/PrescriptionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Main.Controllers
+{
+    public class PrescriptionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
