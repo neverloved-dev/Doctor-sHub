ab033e12762d984e24f7ba1b11f3a8768c8077f9
Andjic Djordje <neverloved.dev@gmail.com>
2024-08-08 23:21:13 +0200

    Started working on Controller tests

--- Main-service/MainTests/PrescriptionTests/PrescriptionControllerTests.cs
+++ Main-service/MainTests/PrescriptionTests/PrescriptionControllerTests.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.TestHost;
 
 namespace MainTests.PrescriptionTests
 {
-    public class PrescriptionControllerTests
+    public class PrescriptionControllerTests:IClassFixture<WebApplicationFactory<Program>>
     {
-        
+        private readonly WebApplicationFactory<Program> _applicationFactory;
+
+        public PrescriptionControllerTests(WebApplicationFactory<Program> client)
+        {
+            _applicationFactory = client;
+        }
     }
 }
