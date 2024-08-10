f1003698202b2a14a165d3482287ec707417e73e
Andjic Djordje <neverloved.dev@gmail.com>
2024-08-11 00:08:32 +0200

    Nearly done with doctor service

---
+++ Main-service/Main/DTOs/CreateDoctorDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Main.DTOs;
+
+public class CreateDoctorDTO
+{
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Specialization { get; set; }
+    public int YearsOfExperience { get; set; }
+}
--- Main-service/Main/Services/DoctorService.cs
+++ Main-service/Main/Services/DoctorService.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Main.DTOs;
+using Main.Models;
 using Main.Repositories;
 
 namespace Main.Services
@@ -15,5 +17,37 @@ namespace Main.Services
         }
 
         //TODO: Implement CRUD operations with pagination using DoctorRepository
+        public GetDoctorDTO? GetSingleDoctor(int doctorId)
+        {
+            GetDoctorDTO? dto = new GetDoctorDTO();
+            var doctor = _doctorRepository.GetSingle(doctorId);
+            if (doctor == null) return null;
+            dto.YearsOfExperience = doctor.YearsOfExperience;
+            dto.Name = doctor.Name;
+            dto.LastName = doctor.LastName;
+            dto.Speciality = doctor.Specialization;
+            return dto;
+
+        }
+
+        public void AddNewDoctor(CreateDoctorDTO requestDto)
+        {
+            Doctor newDoctor = new Doctor();
+            newDoctor.YearsOfExperience = requestDto.YearsOfExperience;
+            newDoctor.Name = requestDto.Name;
+            newDoctor.Specialization = requestDto.Specialization;
+            newDoctor.LastName = requestDto.LastName;
+            _doctorRepository.Create(newDoctor);
+        }
+        
+        public  List<GetDoctorDTO>? GetDoctorsPaginated()
+        {
+            return null;
+        }
+
+        public void DeleteDoctor(int id)
+        {
+            _doctorRepository.Delete(id);
+        }
     }
 }
